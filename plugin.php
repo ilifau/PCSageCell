a82f00d576ac772e41c929e1272513107acd2e9c
Fred Neumann <fred.neumann@fau.de>
2019-07-24 23:28:52 +0200

    Version 1.0.0 for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -12,12 +12,12 @@
 $id = "pcsage";
 
 // code version; must be changed for all code changes
-$version = "0.9.3";
+$version = "1.0.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.3.0";
-$ilias_max_version = "5.3.999";
+$ilias_min_version = "5.4.4";
+$ilias_max_version = "5.4.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jesus Copado";
