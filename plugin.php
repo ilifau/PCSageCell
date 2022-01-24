6063799ac0cc79696ec2cbd49da8eea0dfeac513
Cornel Musielak <cornel.musielak@fau.de>
2022-01-24 07:49:46 +0100

    Version 1.0.3 for ILIAS 7

--- plugin.php
+++ plugin.php
@@ -12,12 +12,12 @@
 $id = "pcsage";
 
 // code version; must be changed for all code changes
-$version = "1.0.2";
+$version = "1.0.3";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.4";
-$ilias_max_version = "6.9.999";
+$ilias_max_version = "7.9.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jesus Copado";
