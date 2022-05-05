3b6bf57d576935b6ca0e59c4507abcd59b0fc041
Fred Neumann <fred.neumann@fau.de>
2022-05-05 11:09:56 +0200

    fixed max. ilias version

--- plugin.php
+++ plugin.php
@@ -17,7 +17,7 @@ $version = "1.0.3";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.4";
-$ilias_max_version = "7.9.999";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jesus Copado";
